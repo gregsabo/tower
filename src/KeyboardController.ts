73320f5eae0bcdf04f512ee28836f0a8c0202b5a
Greg Sabo <gregsabo@asana.com>
2018-10-22 09:12:22 -0400

    Refactor, and handle removed cans with cursors.

--- src/KeyboardController.ts
+++ src/KeyboardController.ts
@@ -16,65 +16,56 @@ export default class KeyboardController {
         });
     }
 
-    public moveCursorUp() {
-        log("Trying to go up.");
+    public findCanAboveCursor() {
         const result = findById(this.app.state.programs, this.app.state.canCursorId);
         if (!result) {
-            log("No result found. Cannot move cursor.");
-            return;
+            log("No result found. Assuming bottom.");
+            return this.app.state.programs[0].uniqueId;
         }
         if (result.invocation.args && result.invocation.args.length > 0) {
-            this.app.setState({
-                canCursorId: result.invocation.args[0].uniqueId
-            });
+            return result.invocation.args[0].uniqueId;
         } else {
-            log("No args, cannot move cursor up.");
+            return null;
         }
     }
 
-    public moveCursorDown() {
+    public findCanBelowCursor() {
         const result = findById(this.app.state.programs, this.app.state.canCursorId);
         if (!result) {
-            log("No result found. Cannot move cursor.");
-            return;
+            log("No result found. Assuming bottom.");
+            return this.app.state.programs[0].uniqueId;
         }
         if (result.path.length === 0) {
             log("Bottom of tower. Cannot go down.");
-            return;
+            return null;
         }
-        this.app.setState({
-            canCursorId: result.path[result.path.length - 1].uniqueId
-        });
+        return result.path[result.path.length - 1].uniqueId;
     }
 
-    public moveCursorLeft() {
+    public findCanToLeftOfCursor() {
         const result = findById(this.app.state.programs, this.app.state.canCursorId);
         if (!result) {
-            log("No result found. Cannot move cursor.");
-            return;
+            log("No result found. Assuming bottom.");
+            return this.app.state.programs[0].uniqueId;
         }
         if (result.path.length === 0) {
             log("Bottom of tower. Cannot go down.");
-            return;
+            return null;
         }
         const parent = result.path[result.path.length - 1];
         const index = parent.args.indexOf(result.invocation);
         if (index === 0) {
             // just move down.
-            return this.app.setState({
-                canCursorId: parent.uniqueId
-            });
+            return parent.uniqueId;
         }
-        this.app.setState({
-            canCursorId: parent.args[index - 1].uniqueId
-        });
+        return parent.args[index - 1].uniqueId;
     }
 
-    public moveCursorRight() {
+    public findCanToRightOfCursor() {
         const result = findById(this.app.state.programs, this.app.state.canCursorId);
         if (!result) {
-            log("No result found. Cannot move cursor.");
-            return;
+            log("No result found. Assuming bottom.");
+            return this.app.state.programs[0].uniqueId;
         }
         if (result.path.length === 0) {
             log("Bottom of tower. Cannot go down.");
@@ -84,27 +75,32 @@ export default class KeyboardController {
         const index = parent.args.indexOf(result.invocation);
         if (index + 1 === parent.args.length) {
             // just move down.
-            return this.app.setState({
-                canCursorId: parent.uniqueId
-            });
+            return parent.uniqueId;
+        }
+        return parent.args[index + 1].uniqueId;
+    }
+
+    public moveCursorTo(uniqueId: string|null) {
+        if (uniqueId === null) {
+            return;
         }
         this.app.setState({
-            canCursorId: parent.args[index + 1].uniqueId
+            canCursorId: uniqueId
         });
     }
 
     private onKeyDown(e: KeyboardEvent) {
         if (e.code === "KeyJ") {
-            this.moveCursorDown();
+            this.moveCursorTo(this.findCanBelowCursor());
         }
         if (e.code === "KeyK") {
-            this.moveCursorUp();
+            this.moveCursorTo(this.findCanAboveCursor());
         }
         if (e.code === "KeyH") {
-            this.moveCursorLeft();
+            this.moveCursorTo(this.findCanToLeftOfCursor());
         }
         if (e.code === "KeyL") {
-            this.moveCursorRight();
+            this.moveCursorTo(this.findCanToRightOfCursor());
         }
 
     }
