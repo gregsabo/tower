66b720b8f2f3b5c1ad5680e1d97f4f649393d558
Greg Sabo <gregsabo@gmail.com>
2018-11-25 13:22:56 -0800

    Better whitespace string display

--- src/Value.tsx
+++ src/Value.tsx
@@ -1,6 +1,6 @@
-import Socket from './Socket';
-import * as React from 'react';
-import { TowerPrimitive } from './Types';
+import Socket from "./Socket";
+import * as React from "react";
+import { TowerPrimitive } from "./Types";
 
 interface IProps {
   value: TowerPrimitive;
@@ -13,11 +13,13 @@ export default function Value(props: IProps) {
 
 function renderResultString(result: TowerPrimitive) {
   if (Socket.describes(result)) {
-    return '<unfilled socket>';
+    return "<unfilled socket>";
   } else if (result instanceof Error) {
     return `Error: ${result.message}`;
   } else if (Array.isArray(result)) {
-    return result.join(', ');
+    return result.join(", ");
+  } else if (typeof result === "string" && result.trim() === "") {
+    return '"' + result + '"';
   } else {
     return result;
   }
